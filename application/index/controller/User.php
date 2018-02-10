48e738d37aedde6d41cf052b304beeafaf413eeb
287970798 <287970798@qq.com>
2018-02-11 00:04:45 +0800

    user 查看 修改 详情

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -4,6 +4,7 @@
 namespace app\index\controller;
 
 use app\index\model\User as UserModel;
+use think\Exception;
 
 class User extends BaseController
 {
@@ -26,14 +27,41 @@ class User extends BaseController
     public function edit()
     {
         $id = $this->request->param('id');
-        $post = $this->request->post();
-        $user = new UserModel();
-        $result = $user->save($post, ['id'=>$id]);
-        return $result;
+        if ($this->request->isPost() && $this->request->isAjax()) {
+            $post = $this->request->post();
+            // 如果有密码并且为空，则不修改
+            if (isset($post['password'])) {
+                if ($post['password'] == '') {
+                    unset($post['password']);
+                } else {
+                    $post['password'] = sha1($post['password']);
+                }
+            }
+            $user = new UserModel();
+            $result = $user->save($post, ['id' => $id]);
+            return $result;
+        }
+        $user = UserModel::get($id);
+        $this->assign([
+            'title' => '用户修改',
+            'user' => $user
+        ]);
+        return $this->fetch();
     }
 
     // delete
-
+    public function del()
+    {
+        if (!$this->request->isDelete()) {
+            return 'error';
+        }
+        $id = $this->request->param('id');
+        if (!is_numeric($id) && !is_int($id)) {
+            return 'error';
+        }
+        $result = UserModel::destroy($id);
+        return $result;
+    }
     // list
     public function all()
     {
@@ -46,6 +74,23 @@ class User extends BaseController
         return $this->fetch();
     }
     // login
-
+    public function login()
+    {
+        if ($this->request->isPost()){
+            $post = $this->request->post();
+            $username = $post['username'];
+            $password = sha1($post['password']);
+            $user = UserModel::where('username', 'eq', $username)->where('password', 'eq', $password)->find();
+            if ($user) {
+                return 1;
+            } else {
+                return 2;
+            }
+        }
+        $this->assign([
+            'title' => '用户登录'
+        ]);
+        return $this->fetch();
+    }
     // logout
 }
--- application/route.php
+++ application/route.php
@@ -5,3 +5,5 @@ use think\Route;
 Route::rule('user/add', 'user/add');
 Route::rule('user/all', 'user/all');
 Route::rule('user/edit/:id', 'user/edit');
+Route::rule('user/del/:id', 'user/del');
+Route::rule('login', 'user/login');
